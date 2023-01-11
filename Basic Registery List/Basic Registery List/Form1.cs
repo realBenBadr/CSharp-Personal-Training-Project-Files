801addd325f2c055f849cdd780739a641c4a299b
realBenBadr <104841823+realBenBadr@users.noreply.github.com>
2023-01-11 20:42:13 +0330

    Add files via upload

---
+++ Basic Registery List/Basic Registery List/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Basic_Registery_List
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> name = new List<string>();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            name.Add(textBox1.Text);
+            name.Sort();
+            listBox1.DataSource = null;
+            listBox1.DataSource= name;
+            textBox1.Text = "";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            name.Remove(textBox1.Text);
+            listBox1.DataSource = null;
+            listBox1.DataSource = name;
+            textBox1.Text = "";
+        }
+
+
+    }
+}
---
+++ training 3 BMI/training 3 BMI/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace training_3_BMI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        
+        double h;
+        double w;
+        
+
+        double BMI(double w, double h) 
+        {
+        
+            return (w /(h * h));
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            h = Convert.ToDouble(textBox1.Text);
+            w = Convert.ToDouble(textBox2.Text);
+
+            label4.Text = BMI(w, h).ToString();
+        }
+    }
+}
