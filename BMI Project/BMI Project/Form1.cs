067636e09d6d57fd65c9ff87ba6d0af770127e03
realBenBadr <104841823+realBenBadr@users.noreply.github.com>
2023-01-11 20:40:38 +0330

    CSharp Personal Training Project Files
    CSharp Personal Training Project Files

---
+++ BMI Project/BMI Project/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Project
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double hight;
+        double weight;
+
+        double BMI(double h, double w) 
+        {
+            return w / (h * h);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            weight = Convert.ToDouble(textBox1.Text);
+            hight = Convert.ToDouble(textBox2.Text);
+
+            label4.Text = BMI(hight, weight).ToString();
+        }
+
+        
+    }
+}
---
+++ Project Calculator/Project Calculator/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double a;
+        double b;
+
+
+
+
+        string Sum(double c, double d)
+        {
+            return Convert.ToString(c+d);
+        }
+        
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            MessageBox.Show(Convert.ToString(a+b));
+            //MessageBox.Show(Sum(a, b));
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            MessageBox.Show(Convert.ToString(a-b));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            MessageBox.Show(Convert.ToString(a*b));
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            a = Convert.ToDouble(textBox1.Text);
+            b = Convert.ToDouble(textBox2.Text);
+            MessageBox.Show(Convert.ToString(a/b));
+        }
+    }
+}
